926944d2766d6f5aeb510444833deacc21d2191a
Ztrolix <xdpxigaming@gmail.com>
2025-02-08 21:31:16 +1100

    1.3.2
    - Improved Logging

--- core/logging.py
+++ core/logging.py
@@ -1,17 +1,27 @@
+from datetime import datetime
+
 from colorama import Fore, Style
 
 
-def debug(message: str):
-    print(f"{Fore.LIGHTBLACK_EX}DEBUG: {Style.RESET_ALL}{message}")
+def get_current_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+def debug(message: str):
+    timestamp = get_current_time()
+    log_message = f"{Fore.LIGHTBLACK_EX}DEBUG: {Style.RESET_ALL}{message}"
+    print(f"{timestamp} - {log_message}")
 
 def info(message: str):
-    print(f"{Style.RESET_ALL}INFO: {message}")
-
+    timestamp = get_current_time()
+    log_message = f"{Style.RESET_ALL}INFO: {message}"
+    print(f"{timestamp} - {log_message}")
 
 def warn(message: str):
-    print(f"{Fore.YELLOW}WARNING: {Style.RESET_ALL}{message}")
-
+    timestamp = get_current_time()
+    log_message = f"{Fore.YELLOW}WARNING: {Style.RESET_ALL}{message}"
+    print(f"{timestamp} - {log_message}")
 
 def error(message: str):
-    print(f"{Fore.RED}ERROR: {Style.RESET_ALL}{message}")
+    timestamp = get_current_time()
+    log_message = f"{Fore.RED}ERROR: {Style.RESET_ALL}{message}"
+    print(f"{timestamp} - {log_message}")
