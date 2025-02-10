d99b1df06d034d4571aa91e82f070ca8df2f55cf
Ztrolix <xdpxigaming@gmail.com>
2025-02-10 17:21:15 +1100

    1.6.0-Beta.3
    - Fully implemented ant spawner tool
    - Fixed food spawner tool crashes

--- tools/ant.py
+++ tools/ant.py
@@ -5,10 +5,11 @@ from entities import worker
 def draw(eventPos, threshold_slider, seed_button, speed_slider, start_button):
     if not settings.ui_visible:
         x, y = eventPos
+        grid_x = (x + settings.camera_x) // 10
+        grid_y = (y + settings.camera_y) // 10
         if not threshold_slider.is_hovered or not seed_button.is_hovered or not speed_slider.is_hovered or not settings.ant_slider.is_hovered or not start_button.is_hovered:
             settings.ant_slider.value += 1
-            settings.ants = [worker.Ant(settings.nest_location[0], settings.nest_location[1], settings.nest_location,
-                                        settings.pheromone_map, speed_slider.value)
-                             for _ in range(int(settings.ant_slider.value))]
+            settings.ants.append(worker.Ant(grid_x, grid_y, settings.nest_location,
+                                settings.pheromone_map, speed_slider.value))
         else:
             settings.drawing_ant = False
